5c72775cbe47591285587e7d992df0b55dc6ec84
Wunder Wungiel <dredlok706@proton.me>
2026-02-04 12:58:26 +0100

    Drop nnhub support, since it's broken

--- app/lib/__init__.py
+++ app/lib/__init__.py
@@ -14,4 +14,3 @@ from .auth import auth as auth_blueprint
 from .applist import applist as applist_blueprint
 from .qtstore import qtstore as qtstore_blueprint
 from .news import news as news_blueprint
-from .nnhub import nnhub as nnhub_blueprint
