bf0932e7674a10fab64ba4b0f3a8b873cf985c35
Wunder Wungiel <dredlok706@proton.me>
2026-02-11 16:09:53 +0100

    Fix Other apps being the default

--- app/lib/store.py
+++ app/lib/store.py
@@ -258,7 +258,8 @@ def _content_type(content_type):
 
 def _content(content_type):
 
-    category_id = request.args.get('category_id', default=1, type=int)
+    category_id = request.args.get('category_id')
+    category_id = int(category_id) if category_id else None
 
     category = db.get_category(category_id, content_type) if category_id else None
 
