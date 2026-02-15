e25f1b8d203324d765e64485bc148a322b497f1f
Wunder Wungiel <dredlok706@proton.me>
2026-02-15 18:08:59 +0100

    Make config module more readable

--- app/lib/config.py
+++ app/lib/config.py
@@ -1,5 +1,7 @@
 import yaml
 import os
 
-with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "config.yaml"), "r") as f:
+path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "..", "config.yaml")
+
+with open(path, "r") as f:
     config = yaml.safe_load(f.read())
